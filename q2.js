293c279d66c2649b6e319a197016c92bf9657fbd
Mathilbzel <buachompoo_r@cmu.ac.th>
2024-07-06 18:02:34 +0700

    test

---
+++ q2.js
@@ -0,0 +1,15 @@
+// define interface for Student object
+/* Your code here */
+// assign interface/type to the function definition properly
+function findTopNames(students) {
+    var topStudents = students.filter(function (student) { return student.score > 8; });
+    return topStudents.map(function (student) { return student.name; }).slice(0, 2);
+}
+// assign interface/type to the student1 object properly
+var students1 = [
+    { name: "john", score: 10 },
+    { name: "jane", score: 9 },
+    { name: "jim", score: 8 },
+];
+console.log(findTopNames(students1));
+module.exports = findTopNames;
--- q2.ts
+++ q2.ts
@@ -3,7 +3,8 @@
 
 // assign interface/type to the function definition properly
 function findTopNames(students) {
-  /* Your code here */
+  const topStudents = students.filter(student => student.score > 8);
+  return topStudents.map(student => student.name).slice(0, 2);
 }
 
 // assign interface/type to the student1 object properly
